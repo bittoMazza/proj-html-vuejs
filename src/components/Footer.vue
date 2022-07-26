b95807f5135e79d25c126170d33847c829a28465
MyUsername <robertomazza99@gmail.com>
2022-07-26 19:42:22 +0200

    add some footer content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,35 @@
 <template>
   <footer class="py-5">
     <div class="container">
-        <div class="col-6">        
-            <ul>
-                <li class="h5"> Address </li>
-                <li> {{this.contactInfo.address}} </li>
-                <li class="my-2"> {{this.contactInfo.phone}}</li>
-                <li> {{this.contactInfo.email}}</li>
-            </ul>
-            <div class="my-2">
-                logo logo logo logo
+        <div class="d-flex">
+            <div class="col-6">        
+                <ul>
+                    <li class="h5"> Address </li>
+                    <li> {{this.contactInfo.address}} </li>
+                    <li class="my-2"> {{this.contactInfo.phone}}</li>
+                    <li> {{this.contactInfo.email}}</li>
+                </ul>
+                <div class="my-2">
+                    logo logo logo logo
+                </div>
             </div>
-        </div>
-        <div class="col-6">
-            
-        </div>
+            <div class="col-6 d-flex">
+                <ul>
+                    <li class="my-3">Prova</li>
+                    <li>dad</li>
+                    <li>dad</li>
+                </ul>
+                <ul>
+                    <li>p</li>
+                </ul>
+                <h6>Information</h6>
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </div>      
     </div>
   </footer>
 </template>
